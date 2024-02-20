7232f5dcb7c9a61ad45dcd53630424d11cf7b95e
orbitlens <orbit.lens@gmail.com>
2024-02-20 16:40:42 -0100

    Update dependencies and prepare for NPM publish

--- src/liquidity-pool-stake.js
+++ src/liquidity-pool-stake.js
@@ -1,20 +1,16 @@
-import Bignumber from 'bignumber.js'
-import {stripTrailingZeros} from '@stellar-expert/formatter'
-
+import {fromStroops} from '@stellar-expert/formatter'
 /**
  * Estimate amounts of liquidity pool tokens based on the share of the pool
- * @param {Number|String|Bignumber} shares - Amount of pool shares that belong to the account
- * @param {Array<String|Number|Bignumber>} reserves - Total amount of tokens deposited to the pool
- * @param {Number|String|Bignumber} totalShares - Total pool shares
- * @return {Array<String>}
+ * @param {Number|String} shares - Amount of pool shares that belong to the account
+ * @param {Array<String|Number>} reserves - Total amount of tokens deposited to the pool
+ * @param {Number|String} totalShares - Total pool shares
+ * @return {String[]}
  */
 export function estimateLiquidityPoolStakeValue(shares, reserves, totalShares) {
-    if (!(shares > 0) || !(totalShares > 0)) return null
+    if (!(shares > 0) || !(totalShares > 0))
+        return null
     return reserves.map(reserve => {
-        const amount = new Bignumber(shares)
-            .mul(new Bignumber(reserve))
-            .div(totalShares)
-            .toFixed(7, Bignumber.ROUND_DOWN)
-        return stripTrailingZeros(amount)
+        const value = BigInt(shares) * BigInt(reserve) * 10000000n / BigInt(totalShares)
+        return fromStroops(value)
     })
 }
